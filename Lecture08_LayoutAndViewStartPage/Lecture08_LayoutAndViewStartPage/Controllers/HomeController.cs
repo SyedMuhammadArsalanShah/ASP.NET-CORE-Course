039d642234ba6ac302a05989184ac1190eb0d638
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2024-06-20 20:38:49 +0500

    lect 07

---
+++ Lecture08_LayoutAndViewStartPage/Lecture08_LayoutAndViewStartPage/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lecture08_LayoutAndViewStartPage.Controllers
+{
+	public class HomeController : Controller
+	{
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+
+
+		public IActionResult About()
+		{
+			return View();
+		}
+
+
+		public IActionResult Service()
+		{
+			return View();
+		}
+	}
+}
